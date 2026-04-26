8da0e1a6ea8de6d9363640203f7ac6bcd8936861
何彦 <2838159002@qq.com>
2026-04-26 21:54:35 +0800

    fix: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://your-blog.pages.dev',
+  site: 'https://echoofher.pages.dev',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
